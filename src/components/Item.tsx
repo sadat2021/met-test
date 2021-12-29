8a595ddffbd707394ee031339461095f01dcc0af
Sadat2021 <Azade.s.it@gmail.com>
2021-12-29 17:00:18 +1100

    add props to Item

--- src/components/Item.tsx
+++ src/components/Item.tsx
@@ -1,20 +1,28 @@
 import React from "react";
+import NoImage from "./NoImage";
 
-export default function Item() {
+interface ItemProps {
+  title: string;
+  department: string;
+  primaryImage: string;
+}
+
+export default function Item({ primaryImage, title, department }: ItemProps) {
   return (
-    <div className="w-100 lg:w-1/5 md:w-1/4 sm:w-1/2 p-4">
+    <div className="w-100 lg:w-1/4 md:w-1/4 sm:w-1/2 p-4">
       <div className="container flex flex-col bg-white shadow-md rounded-md cursor-pointer">
-        <img
-          src="https://images.metmuseum.org/CRDImages/ad/web-large/130480.jpg"
-          alt="test"
-          className="w-full rounded-t-md"
-        />
-        <h3 className="m-2 text-lg font-bold">Andiron</h3>
+        {primaryImage === "" ? (
+          <NoImage title={title} />
+        ) : (
+          <img src={primaryImage} alt={title} className="w-full rounded-t-md" />
+        )}
+
+        <h3 className="m-2 text-lg font-bold">{title}</h3>
 
         <div className="flex flex-row flex-wrap mx-2 mb-2">
           <span className="w-1/3 text-sm">Department:</span>
           <span className="w-2/3 text-sm font-bold text-right">
-            The American Wing
+            {department}
           </span>
         </div>
       </div>
