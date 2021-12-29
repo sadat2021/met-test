4739363ce898cc3567198b1d4224f80b4a09a940
Sadat2021 <Azade.s.it@gmail.com>
2021-12-29 17:00:36 +1100

    create NoImage components

---
+++ src/components/NoImage.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import "./noImage.css";
+
+interface NoImageProps {
+  title: string;
+}
+
+export default function NoImage({ title }: NoImageProps) {
+  return (
+    <div className="bg-gray-200 flex-1 flex justify-center items-center noimage">
+      <span className="text-lg font-bold text-slate-400">
+        {title.substring(0, 2).toUpperCase()}
+      </span>
+    </div>
+  );
+}
