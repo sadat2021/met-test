c5c6e7b83e444f58244c64015b60369afc91b0da
Sadat2021 <Azade.s.it@gmail.com>
2021-12-29 15:01:04 +1100

    add Header

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
 import React from "react";
+import Header from "./components/Header";
 
 function App() {
   return (
     <div className="container bg-gray-100 min-h-screen">
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      <Header />
     </div>
   );
 }
---
+++ src/components/Header.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Header() {
+  return (
+    <div className="container flex items-center justify-center p-4 bg-sky-500 fixed shadow-sm">
+      <h1 className="text-2xl text-white">MET APP</h1>
+    </div>
+  );
+}
