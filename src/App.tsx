d479ed16260552b2c25dec1ddd484631a203616d
Sadat2021 <Azade.s.it@gmail.com>
2021-12-29 14:59:41 +1100

    add tailwind to project

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,9 @@
-import React from 'react';
+import React from "react";
 
 function App() {
   return (
-    <div className="App">
-      met
+    <div className="container bg-gray-100 min-h-screen">
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
     </div>
   );
 }
